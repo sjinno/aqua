c2652c83310e146dd9da6b0f4d8c8bfe90f9074b
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-17 21:35:03 -0700

    Take user inputs to set interval and number of repeats.

--- warnme.sh
+++ warnme.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 counter=1
-while [ $counter -le 10 ]
+echo "Set interval. (For example, 20 for every 20 mins.)" ; read interval
+echo "Set number of repetitions. (For example, 10 will repeat 10 times)" ; read num
+echo "Program has started :)"
+while [ $counter -le $num ]
 do  
     bold=$(tput bold)
     normal=$(tput sgr0)
-    sleep 30m
+    sleep ${interval}m
     time=$(date +%H:%M:%S)
-    echo "$counter: ${bold}$time${normal}" ; play -q water.mp3
+    echo "${counter}: ${bold}${time}${normal}" ; play -q water.mp3
     ((counter++))
 done
 
-echo Done
+echo "Program has ended."
