ee2f27171c3aacd664ddafd4a9f6c55471f9b926
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-17 23:07:37 -0700

    Format.

--- warnme.sh
+++ warnme.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "Set interval.     ${bold}Example: 20${normal} # Every 20 mins" ; read interval
+echo "Set # of repeats. ${bold}Example: 10${normal} # Repeat 10 times" ; read num
+
 counter=1
-echo "Set interval. (For example, 20 for every 20 mins.)" ; read interval
-echo "Set number of repetitions. (For example, 10 will repeat 10 times)" ; read num
 echo "Program has started :)"
 while [ $counter -le $num ]
-do  
-    bold=$(tput bold)
-    normal=$(tput sgr0)
+do
     sleep ${interval}m
     time=$(date +%H:%M:%S)
     echo "${counter}: ${bold}${time}${normal}" ; play -q water.mp3
     ((counter++))
 done
-
 echo "Program has ended."
